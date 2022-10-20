35820bf0b14289f4604f310200499a4dba68757a
Jaewon <woin2ee@gmail.com>
2022-10-20 23:32:07 +0900

    [refactor] MainTabBarController 분리, 필요없는 주석 삭제, 코드 일관적으로 정렬, 파일 그룹화

--- own-exhibition/Application/SceneDelegate.swift
+++ own-exhibition/Application/SceneDelegate.swift
@@ -8,77 +8,48 @@
 import UIKit
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
-
+    
     var window: UIWindow?
-
-
+    
     func scene(
         _ scene: UIScene,
         willConnectTo session: UISceneSession,
         options connectionOptions: UIScene.ConnectionOptions
     ) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
-        
         window = UIWindow.init(windowScene: windowScene)
         
-        let homeVC = HomeViewController.instantiate(withStoryboardName: "Home")
-        let homeTabBarItem = UITabBarItem.init()
-        homeVC.tabBarItem = homeTabBarItem
-        
-//        let alphaTabBarItem = UITabBarItem.init(
-//            title: "Alpha",
-//            image: UIImage.init(systemName: "a.square"),
-//            selectedImage: UIImage.init(systemName: "a.square.fill")
-//        )
-//        alphaVC.tabBarItem = alphaTabBarItem
-        
-        
-//        let betaVC = BetaViewController.init()
-//
-//        let betaTabBarItem = UITabBarItem.init(
-//            title: "Beta",
-//            image: UIImage.init(systemName: "b.square"),
-//            selectedImage: UIImage.init(systemName: "b.square.fill")
-//        )
-//        betaVC.tabBarItem = betaTabBarItem
-        
-        let tabBarController = UITabBarController.init()
-        tabBarController.viewControllers = [homeVC]
-        tabBarController.tabBar.backgroundColor = .systemGray6
+        let tabBarController = MainTabBarController.init()
         
         window?.rootViewController = tabBarController
-        
         window?.makeKeyAndVisible()
     }
-
+    
     func sceneDidDisconnect(_ scene: UIScene) {
         // Called as the scene is being released by the system.
         // This occurs shortly after the scene enters the background, or when its session is discarded.
         // Release any resources associated with this scene that can be re-created the next time the scene connects.
         // The scene may re-connect later, as its session was not necessarily discarded (see `application:didDiscardSceneSessions` instead).
     }
-
+    
     func sceneDidBecomeActive(_ scene: UIScene) {
         // Called when the scene has moved from an inactive state to an active state.
         // Use this method to restart any tasks that were paused (or not yet started) when the scene was inactive.
     }
-
+    
     func sceneWillResignActive(_ scene: UIScene) {
         // Called when the scene will move from an active state to an inactive state.
         // This may occur due to temporary interruptions (ex. an incoming phone call).
     }
-
+    
     func sceneWillEnterForeground(_ scene: UIScene) {
         // Called as the scene transitions from the background to the foreground.
         // Use this method to undo the changes made on entering the background.
     }
-
+    
     func sceneDidEnterBackground(_ scene: UIScene) {
         // Called as the scene transitions from the foreground to the background.
         // Use this method to save data, release shared resources, and store enough scene-specific state information
         // to restore the scene back to its current state.
     }
-
-
 }
-
