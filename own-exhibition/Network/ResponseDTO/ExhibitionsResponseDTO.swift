4601e9ca4141faf51fad44630c3380d9b298e5b9
Jaewon Yun <woin2ee@gmail.com>
2022-11-22 18:44:14 +0900

    [Feat] JSON 디코딩 중 타입 미스매치 시 빈 값으로 초기화 되도록 구현

--- own-exhibition/Network/ResponseDTO/ExhibitionsResponseDTO.swift
+++ own-exhibition/Network/ResponseDTO/ExhibitionsResponseDTO.swift
@@ -21,6 +21,49 @@ final class ExhibitionResponseDTO: Decodable {
     let area: String
     let thumbnail: String
     let gpsX, gpsY: String
+    
+    enum CodingKeys: CodingKey {
+        case seq
+        case title
+        case startDate
+        case endDate
+        case place
+        case realmName
+        case area
+        case thumbnail
+        case gpsX
+        case gpsY
+    }
+    
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        self.seq = try container.decode(String.self, forKey: .seq)
+        self.title = try container.decode(String.self, forKey: .title)
+        self.startDate = try container.decode(String.self, forKey: .startDate)
+        self.endDate = try container.decode(String.self, forKey: .endDate)
+        if let place = try? container.decode(String.self, forKey: .place) {
+            self.place = place
+        } else {
+            self.place = ""
+        }
+        self.realmName = try container.decode(String.self, forKey: .realmName)
+        if let area = try? container.decode(String.self, forKey: .area) {
+            self.area = area
+        } else {
+            self.area = ""
+        }
+        self.thumbnail = try container.decode(String.self, forKey: .thumbnail)
+        if let gpsX = try? container.decode(String.self, forKey: .gpsX) {
+            self.gpsX = gpsX
+        } else {
+            self.gpsX = ""
+        }
+        if let gpsY = try? container.decode(String.self, forKey: .gpsY) {
+            self.gpsY = gpsY
+        } else {
+            self.gpsY = ""
+        }
+    }
 }
 
 extension ExhibitionResponseDTO: EntityConvertible {
@@ -31,13 +74,13 @@ extension ExhibitionResponseDTO: EntityConvertible {
             startDate: {
                 let startDate: String = startDate
                 let formatter = DateFormatter()
-                formatter.dateFormat = "yyyy.MM.dd"
+                formatter.dateFormat = "yyyyMMdd"
                 return formatter.date(from: startDate)!
             }(),
             endDate: {
                 let startDate: String = endDate
                 let formatter = DateFormatter()
-                formatter.dateFormat = "yyyy.MM.dd"
+                formatter.dateFormat = "yyyyMMdd"
                 return formatter.date(from: startDate)!
             }(),
             location: .init(lat: Double(gpsX) ?? 0, lon: Double(gpsY) ?? 0),
