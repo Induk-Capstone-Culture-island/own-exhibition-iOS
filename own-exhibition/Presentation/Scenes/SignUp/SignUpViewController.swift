6ce28c1ccbb1bdb36b9ac82329c10756a1318b54
Jaewon Yun <woin2ee@gmail.com>
2022-11-22 15:08:56 +0900

    [Feat] 회원가입 화면 UI 구성 완료

--- own-exhibition/Presentation/Scenes/SignUp/SignUpViewController.swift
+++ own-exhibition/Presentation/Scenes/SignUp/SignUpViewController.swift
@@ -9,23 +9,52 @@ import UIKit
 import RxSwift
 
 final class SignUpViewController: UIViewController {
-
+    
     private var disposeBag: DisposeBag = .init()
     
     private var viewModel: SignUpViewModel!
     
+    // MARK: - UI Components
+    
+    @IBOutlet weak var inputFormStackView: UIStackView! {
+        didSet {
+            inputFormStackView.arrangedSubviews.forEach { view in
+                view.layer.addBorder([.bottom], color: .systemGray4, width: 1.0)
+            }
+        }
+    }
+    @IBOutlet weak var idTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField! {
+        didSet { passwordTextField.isSecureTextEntry = true }
+    }
+    @IBOutlet weak var repasswordTextField: UITextField! {
+        didSet { repasswordTextField.isSecureTextEntry = true }
+    }
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var birthdayDatePicker: UIDatePicker!
+    @IBOutlet weak var signUpButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-    }
-    
-    func bindViewModel() {
-        let input = SignUpViewModel.Input.init()
-        let output = viewModel.transform(input: input)
-
-        _ = output
+        bindViewModel()
+        configureNavigationBar()
     }
     
     func setViewModel(by viewModel: SignUpViewModel) {
         self.viewModel = viewModel
+    }
+}
+
+private extension SignUpViewController {
+    
+    func bindViewModel() {
+        let input = SignUpViewModel.Input.init()
+        let output = viewModel.transform(input: input)
+        
+        _ = output
+    }
+    
+    func configureNavigationBar() {
+        self.navigationItem.title = "회원가입"
     }
 }
