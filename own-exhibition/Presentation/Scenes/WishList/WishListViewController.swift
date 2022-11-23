5df1961dcd16098bfbb2707aca7804c53cfa5550
Jaewon Yun <woin2ee@gmail.com>
2022-11-23 18:59:17 +0900

    [Feat] WishList Scene 관련 파일 생성

---
+++ own-exhibition/Presentation/Scenes/WishList/WishListViewController.swift
@@ -0,0 +1,15 @@
+//
+//  WishListViewController.swift
+//  own-exhibition
+//
+//  Created by Jaewon Yun on 2022/11/23.
+//
+
+import UIKit
+
+final class WishListViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
---
+++ own-exhibition/Presentation/Scenes/WishList/WishListViewModel.swift
@@ -0,0 +1,23 @@
+//
+//  WishListViewModel.swift
+//  own-exhibition
+//
+//  Created by Jaewon Yun on 2022/11/23.
+//
+
+import Foundation
+
+final class WishListViewModel: ViewModelType {
+    
+    struct Input {
+        
+    }
+    
+    struct Output {
+        
+    }
+    
+    func transform(input: Input) -> Output {
+        return .init()
+    }
+}
