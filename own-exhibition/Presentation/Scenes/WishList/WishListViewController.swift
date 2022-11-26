01fee508f25ece40e388df02dc536f022e8abd73
Jaewon Yun <woin2ee@gmail.com>
2022-11-26 19:59:04 +0900

    [Feat] 찜목록 화면 검색바 - ViewModel 바인딩

--- own-exhibition/Presentation/Scenes/WishList/WishListViewController.swift
+++ own-exhibition/Presentation/Scenes/WishList/WishListViewController.swift
@@ -12,6 +12,12 @@ final class WishListViewController: UIViewController {
     
     private let disposeBag = DisposeBag.init()
     
+    @IBOutlet weak var searchBar: UISearchBar! {
+        didSet {
+            searchBar.searchBarStyle = .minimal
+            searchBar.placeholder = "전시회 제목, 지역"
+        }
+    }
     @IBOutlet weak var exhibitionTableView: UITableView!
     
     private var viewModel: WishListViewModel!
@@ -42,7 +48,8 @@ private extension WishListViewController {
         
         let input = WishListViewModel.Input.init(
             viewWillAppear: viewWillAppear.asSignal(onErrorSignalWith: .empty()),
-            selection: exhibitionTableView.rx.itemSelected.asDriver()
+            selection: exhibitionTableView.rx.itemSelected.asDriver(),
+            searchWord: searchBar.rx.text.orEmpty.asDriver().debounce(.milliseconds(300))
         )
         let output = viewModel.transform(input: input)
         
